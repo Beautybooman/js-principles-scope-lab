21d1cd535b485061abb0eba7969f373852cd4ac3
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-03-29 15:34:00 -0400

    commit tree 8cf0505bf408ac3ece1484e0f8f42d08d5847ef3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = ('bob')
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase() 
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(name){
+    return bestCustomer = name;
+}
+
+const leastFavoriteCustomer = 'Murphy' 
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Lord'
+}
